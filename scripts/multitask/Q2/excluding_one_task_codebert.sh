f4c90cfbb44de8a98b7ef8d7de94743edc5dfc62
Otavio-Parraga <otavio-parraga@acad.pucrs.br>
2022-11-17 17:10:35 -0300

    Add script planning for future runs

--- scripts/multitask/Q2/excluding_one_task_codebert.sh
+++ scripts/multitask/Q2/excluding_one_task_codebert.sh
@@ -1,14 +1,14 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-PTM="Salesforce/codet5-base"
-PTM_NAME="Salesforce-codet5-base"
+PTM="microsoft/codebert-base"
+PTM_NAME="microsoft-codebert-base"
 
-# =========================================== 50k ================================================
-EXP_NAME="ablation_50k_iters"
+# =========================================== REMOVING CLONE ================================================
+EXP_NAME="removing_clone"
 CHECKPOINT_PATH="/home/parraga/projects/_masters/multitask_code/checkpoints/multitask/${EXP_NAME}/${PTM_NAME}/best_model.ckpt"
 
 # Train
-CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 12 --gpus 2 --prefix \
---tasks codesearch code2test clone generation defect refine translate summarization -i 50000 \
+CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 10 --gpus 2 --prefix \
+--tasks codesearch code2test generation defect refine translate summarization -i 10000 \
 --cs_lang javascript ruby go java python php --sum_lang javascript ruby go java python php \
 --output_dir $EXP_NAME
 
@@ -22,13 +22,13 @@ CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang ru
 # Eval on code2test
 CUDA_VISIBLE_DEVICES=0 python eval_code2test.py -ptm $PTM -ckpt $CHECKPOINT_PATH -mt --prefix
 
-# =========================================== 100k ================================================
-EXP_NAME="ablation_100k_iters"
+# =========================================== REMOVING GENERATION ================================================
+EXP_NAME="removing_generation"
 CHECKPOINT_PATH="/home/parraga/projects/_masters/multitask_code/checkpoints/multitask/${EXP_NAME}/${PTM_NAME}/best_model.ckpt"
 
 # Train
-CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 12 --gpus 2 --prefix \
---tasks codesearch code2test clone generation defect refine translate summarization -i 100000 \
+CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 10 --gpus 2 --prefix \
+--tasks codesearch code2test clone defect refine translate summarization -i 10000 \
 --cs_lang javascript ruby go java python php --sum_lang javascript ruby go java python php \
 --output_dir $EXP_NAME
 
@@ -42,13 +42,13 @@ CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang ru
 # Eval on code2test
 CUDA_VISIBLE_DEVICES=0 python eval_code2test.py -ptm $PTM -ckpt $CHECKPOINT_PATH -mt --prefix
 
-# =========================================== 150k ================================================
-EXP_NAME="ablation_150k_iters"
+# =========================================== REMOVING DEFECT ================================================
+EXP_NAME="removing_defect"
 CHECKPOINT_PATH="/home/parraga/projects/_masters/multitask_code/checkpoints/multitask/${EXP_NAME}/${PTM_NAME}/best_model.ckpt"
 
 # Train
-CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 12 --gpus 2 --prefix \
---tasks codesearch code2test clone generation defect refine translate summarization -i 150000 \
+CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 10 --gpus 2 --prefix \
+--tasks codesearch code2test clone generation refine translate summarization -i 10000 \
 --cs_lang javascript ruby go java python php --sum_lang javascript ruby go java python php \
 --output_dir $EXP_NAME
 
@@ -62,14 +62,54 @@ CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang ru
 # Eval on code2test
 CUDA_VISIBLE_DEVICES=0 python eval_code2test.py -ptm $PTM -ckpt $CHECKPOINT_PATH -mt --prefix
 
-# =========================================== 200k ================================================
-EXP_NAME="ablation_200k_iters"
+# =========================================== REMOVING REFINE ================================================
+EXP_NAME="removing_refine"
 CHECKPOINT_PATH="/home/parraga/projects/_masters/multitask_code/checkpoints/multitask/${EXP_NAME}/${PTM_NAME}/best_model.ckpt"
 
 # Train
-CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 12 --gpus 2 --prefix \
---tasks codesearch code2test clone generation defect refine translate summarization -i 200000 \
+CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 10 --gpus 2 --prefix \
+--tasks codesearch code2test clone generation defect translate summarization -i 10000 \
 --cs_lang javascript ruby go java python php --sum_lang javascript ruby go java python php \
+--output_dir $EXP_NAME
+
+# Eval on codeserach
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang go -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang java -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang javascript -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang php -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang python -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang ruby -ptm $PTM -mt --prefix
+# Eval on code2test
+CUDA_VISIBLE_DEVICES=0 python eval_code2test.py -ptm $PTM -ckpt $CHECKPOINT_PATH -mt --prefix
+
+# =========================================== REMOVING TRANSLATE ================================================
+EXP_NAME="removing_translate"
+CHECKPOINT_PATH="/home/parraga/projects/_masters/multitask_code/checkpoints/multitask/${EXP_NAME}/${PTM_NAME}/best_model.ckpt"
+
+# Train
+CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 10 --gpus 2 --prefix \
+--tasks codesearch code2test clone generation defect refine summarization -i 10000 \
+--cs_lang javascript ruby go java python php --sum_lang javascript ruby go java python php \
+--output_dir $EXP_NAME
+
+# Eval on codeserach
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang go -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang java -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang javascript -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang php -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang python -ptm $PTM -mt --prefix
+CUDA_VISIBLE_DEVICES=0 python eval_codesearch.py -ckpt $CHECKPOINT_PATH -lang ruby -ptm $PTM -mt --prefix
+# Eval on code2test
+CUDA_VISIBLE_DEVICES=0 python eval_code2test.py -ptm $PTM -ckpt $CHECKPOINT_PATH -mt --prefix
+
+# =========================================== REMOVING SUMMARIZATION ================================================
+EXP_NAME="removing_summarization"
+CHECKPOINT_PATH="/home/parraga/projects/_masters/multitask_code/checkpoints/multitask/${EXP_NAME}/${PTM_NAME}/best_model.ckpt"
+
+# Train
+CUDA_VISIBLE_DEVICES=0,2 python main_multitask.py -ptm $PTM --batch_size 10 --gpus 2 --prefix \
+--tasks codesearch code2test clone generation defect refine translate -i 10000 \
+--cs_lang javascript ruby go java python php \
 --output_dir $EXP_NAME
 
 # Eval on codeserach
